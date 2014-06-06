01e185b3d9901a8b193ed254878c49627aca3922
Georgi Kerezov <assimilation@abv.bg>
2014-06-06 15:27:55 +0300

    Implemented some functionalities

--- 1101561009_GeorgiKerezov/public/core.js
+++ 1101561009_GeorgiKerezov/public/core.js
@@ -4,39 +4,133 @@ var fmiFe = angular.module('fmiFe', []);
 
 function mainController($scope, $http) {
 	$scope.formData = {};
-
-	// when landing on the page, get all todos and show them
-	$http.get('/api/todos')
+	$scope.newField = {};
+	$scope.predicate = '';
+	$scope.typePredicate = '';
+	var regexForDigits = /^\d{10,}$/;
+	
+	$http.get('/api/phoneBook')
 		.success(function(data) {
-			$scope.todos = data;
-			console.log(data);
+			$scope.entries = data;
 		})
 		.error(function(data) {
 			console.log('Error: ' + data);
+	});
+
+	$scope.createEntry = function() {
+		if (Object.keys($scope.formData).length == 3){
+			var match = $scope.formData.Number.match(regexForDigits);
+			if (match != null){
+				$http.post('/api/phoneBook', $scope.formData)
+					.success(function(data) {
+						$scope.formData = {};
+						$scope.entries = data;
+					})
+					.error(function(data) {
+						console.log('Error: ' + data);
+					});
+				clearErrors();
+				clearSuccess();
+				showSuccess();					
+			}else {
+				clearErrors();
+				showErrors();
+			}
+		}
+	};
+
+	$scope.deleteEntry = function(id) {
+		if (confirm('Are you sure you want to delete that entry?')){
+			$http.delete('/api/phoneBook/'+ id)
+				.success(function(data) {
+					$scope.entries = data;
+				})
+				.error(function(data) {
+					console.log('Error: ' + data);
+				});
+		}
+	};
+	
+	$scope.editEntry = function(field) {
+        $scope.newField = angular.copy(field);
+    }
+    
+    $scope.saveEntry = function(entry) {
+		var match = entry.Number.match(regexForDigits);
+		if (match != null){
+            $http.put('/api/phoneBook/'+ entry._id, entry)
+				.success(function(data) {
+					$scope.entries = data;
+				})
+				.error(function(data) {
+					console.log('Error: ' + data);
+				});
+			editMode = false;
+			clearSuccess();
+			showSuccess();
+		}else {
+				editMode = true;
+				clearErrors();
+				showErrors();
+		}
+    };
+    
+    $scope.cancel = function() {
+		$http.get('/api/phoneBook')
+			.success(function(data) {
+				$scope.entries = data;
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
 		});
+		clearErrors();
+    };
+	
+	$scope.changePredicate = function() {
+		if($('#sortOrder option:selected').val() == "asc") {
+			$scope.predicate = $scope.typePredicate;
+		} else {
+			$scope.predicate = "-" + $scope.typePredicate;
+		}
+    };
+	
+	$scope.changeField = function() {
+		if($('#sortField option:selected').val() == "fName") {
+			$scope.typePredicate = "Fname";
+		} else {
+			$scope.typePredicate = "Lname";
+		}	
+    };
+}
 
-	// when submitting the add form, send the text to the node API
-	$scope.createTodo = function() {
-		$http.post('/api/todos', $scope.formData)
-			.success(function(data) {
-				$scope.formData = {}; // clear the form so our user is ready to enter another
-				$scope.todos = data;
-				console.log(data);
-			})
-			.error(function(data) {
-				console.log('Error: ' + data);
-			});
-	};
+function clearErrors() {
+	$('.errors').css("visibility", "hidden");
+	$('.errors').empty();
+}
 
-	// delete a todo after checking it
-	$scope.deleteTodo = function(id) {
-		$http.delete('/api/todos/' + id)
-			.success(function(data) {
-				$scope.todos = data;
-				console.log(data);
-			})
-			.error(function(data) {
-				console.log('Error: ' + data);
-			});
-	};
+function showErrors() {
+	$('.errors').css("visibility", "visible");
+	$('.errors').fadeIn(0);
+	$('.errors').append('Please enter a valid phonenumber that is 10 digits or more');
+	
+	setTimeout( function(){
+		$('.errors').fadeOut(1500);
+	}
+	, 2000 );
+}
+
+function clearSuccess() {
+	$('.success').css("visibility", "hidden");
+	$('.success').empty();
+}
+
+function showSuccess() {
+	$('.success').css("visibility", "visible");
+	$('.success').fadeIn(0);
+	$('.success').append('Success');
+	
+	setTimeout( function(){
+		$('.success').fadeOut(1200);
+	}
+	, 1200 );
 }
--- 1101561009_GeorgiKerezov/server.js
+++ 1101561009_GeorgiKerezov/server.js
@@ -9,7 +9,7 @@
 
 //	mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'); 	// connect to mongoDB database on modulus.io
 
-	mongoose.connect('mongodb://localhost/test');
+	mongoose.connect('mongodb://localhost/localPhoneBook');
 	
 	app.configure(function() {
 		app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
@@ -18,85 +18,103 @@
 	});
 	
 	// 2) Mongo model
-	// define model ================= That is all we want. Just the text for the todo. MongoDB will automatically generate an _id for each todo that we create also.
-	var Todo = mongoose.model('Todo', {
-		text : String
+	// define model ================= That is all we want. Just the text for the PhoneBook. MongoDB will automatically generate an _id for each PhoneBook that we create also.
+	var PhoneBook = mongoose.model('PhoneBook', {
+		Fname : String,
+		Lname : String,
+		Number : String
 	});
 	
 	// 3) routes ======================================================================
 
 	// api ---------------------------------------------------------------------
-	// get all todos
-	app.get('/api/todos', function(req, res) {
-
-		// use mongoose to get all todos in the database
-		Todo.find(function(err, todos) {
+	// get all PhoneBooks
+	app.get('/api/phoneBook', function(req, res) {
+		// use mongoose to get all PhoneBooks in the database
+		PhoneBook.find(function(err, PhoneBooks) {
 
 			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
 			if (err)
 				res.send(err)
 
-			res.json(todos); // return all todos in JSON format
+			res.json(PhoneBooks); // return all PhoneBooks in JSON format
 		});
 	});
 
 	// --------------------- Start Extra Update --------------------------
-	app.get('/api/todos/:todo_id', function(req, res) {
+	/*app.get('/api/phoneBook/:PhoneBook_id', function(req, res) {
 
-		// use mongoose to get all todos in the database
-		Todo.findOne({_id: req.params.todo_id}, '_id text done', function(err, todo) {
+		// use mongoose to get all PhoneBooks in the database
+		PhoneBook.findOne({_id: req.params.PhoneBook_id}, '_id text done', function(err, PhoneBook) {
 
 			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
 			if (err)
 				res.send(err)
 
-			res.json(todo); // return all todos in JSON format
+			res.json(PhoneBook); // return all PhoneBooks in JSON format
 		});
-	});
+	});*/
 	// ----------------------- End Extra Update --------------------------
 	
-	// create todo and send back all todos after creation
-	app.post('/api/todos', function(req, res) {
-
-		// create a todo, information comes from AJAX request from Angular
-		Todo.create({
-			text : req.body.text,
-			done : false
-		}, function(err, todo) {
-			if (err)
+	// create PhoneBook and send back all PhoneBooks after creation
+	app.post('/api/phoneBook', function(req, res) {
+		// create a PhoneBook, information comes from AJAX request from Angular
+		PhoneBook.create({
+			Fname : req.body.Fname,
+			Lname : req.body.Lname,
+			Number : req.body.Number
+		}, function(err, addedRow){
+			if (err){
 				res.send(err);
-
-			// get and return all the todos after you create another
-			Todo.find(function(err, todos) {
-				if (err)
-					res.send(err)
-				res.json(todos);
+			}
+			//find accepts a function as a callback function with which it returns the entries found with a json format
+			//we are searching through the object PhoneBook(our connection with the database) to find the current entries and send them as json.
+			PhoneBook.find(function(err, entries){
+				if (err){
+					res.send(err);
+				}
+				res.json(entries);
 			});
 		});
-
 	});
 
-	// delete a todo
-	app.delete('/api/todos/:todo_id', function(req, res) {
-		Todo.remove({
-			_id : req.params.todo_id
-		}, function(err, todo) {
-			if (err)
-				res.send(err);
-
-			// get and return all the todos after you create another
-			Todo.find(function(err, todos) {
-				if (err)
-					res.send(err)
-				res.json(todos);
+	// delete a PhoneBook
+	app.delete('/api/phoneBook/:PhoneBook_id', function(req, res) {
+	//req.params = contains the parameters in the url(request);
+		PhoneBook.remove({_id: req.params.PhoneBook_id}, 
+			function(err){
+				if (err){
+					res.send(err);
+				}
+				PhoneBook.find(function(err, entries){
+					if (err){
+						res.send(err);
+					}
+					res.json(entries);
+				});
+			}
+		);
+	});
+	
+	app.put('/api/phoneBook/:PhoneBook_id', function(req, res) {
+		PhoneBook.update({ _id: req.body._id },{Fname: req.body.Fname, Lname: req.body.Lname, Number: req.body.Number},
+		function(err){
+				if (err){
+					res.send(err);
+				}
+				PhoneBook.find(function(err, entries){
+					if (err){
+						res.send(err);
+					}
+					res.json(entries);
+				});
 			});
-		});
 	});
 	// Even without it works	
 	// application -------------------------------------------------------------
-	app.get('*', function(req, res) {
+	/*app.get('*', function(req, res) {
 		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-	});
+	});*/
 	
 	// listen (start app with node server.js) ======================================
 	app.listen(8081);
